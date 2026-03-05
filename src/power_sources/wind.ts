0bbc94d3cee414723afd1e9e215d6369e91e6a8e
Zachary Kubli <kubli.zachary@gmail.com>
2026-03-05 12:07:11 -0500

    collab coding changes restored

--- src/power_sources/wind.ts
+++ src/power_sources/wind.ts
@@ -2,10 +2,10 @@
 import { PowerSource } from './power_source'
 import { World } from '../world'
 
-export class Solar extends PowerSource {
+export class Wind extends PowerSource {
     static MAX_MW: number = 1000
 
     updateProduction(deltaTime: number, world: World): number {
-        this.inputMw = world.getDaylightFactor() * world.getCloudiness() * this.MAX_MW
+        this.inputMw = world.getWindiness() * this.MAX_MW
     }
 }
